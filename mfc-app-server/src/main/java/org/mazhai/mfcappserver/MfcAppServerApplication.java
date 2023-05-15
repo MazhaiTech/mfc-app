48dc3bd8730853aa4a23ce63ae832cb47ac1f903
Dhamodaran <dhamo@gmail.com>
2023-05-15 21:41:23 +0530

    Initial Commit to Server

---
+++ mfc-app-server/src/main/java/org/mazhai/mfcappserver/MfcAppServerApplication.java
@@ -0,0 +1,13 @@
+package org.mazhai.mfcappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfcAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfcAppServerApplication.class, args);
+	}
+
+}
---
+++ mfc-app-server/src/test/java/org/mazhai/mfcappserver/MfcAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mazhai.mfcappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfcAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
