3efecf62c067bba05197c67566ed4ff96949764d
aslin-r <aslinjoelr123@gmail.com>
2023-05-21 23:53:00 +0530

    aligned first and last name in horizontal

--- client/src/Register.js
+++ client/src/Register.js
@@ -6,47 +6,46 @@ export default function Register(){
 
         <form>
 
-            <div className='form-group row'>
-                <label className='col-sm-2 col-form-label'>First name</label>
-                <div className='col-sm-10'>
+            <div className='form-group row mt-2'>
+                
+                <div className='col-md-5 mb-3'>
+                <label className=' col-form-label'>First name</label>
                     <input type="text" className='form-control' name="firstname" placeholder="First name" />
                 </div>
-             </div>
-            
-             <div className='form-group row'>
-                <label className='col-sm-2 col-form-label'>Last name</label>
-                <div className='col-sm-10'>
-                    <input type="text" className='form-control' name="lastname" placeholder="Last name" />
+             
+             <div className='col-md-5 mb-3'>
+                <label className=' col-form-label'>Last name</label>
+                    <input type="text" className='form-control' name="lastname" placeholder="Last Name" />
                 </div>
              </div>
             
-             <div className='form-group row'>
+             <div className='form-group row mt-2'>
                 <label className='col-sm-2 col-form-label'>Email</label>
-                <div className='col-sm-10'>
-                    <input type="email" className='form-control' name="email" placeholder="Email name" />
+                <div className='col-sm-5'>
+                    <input type="email" className='form-control' name="email" placeholder="Email" />
                 </div>
              </div>
 
-             <div className='form-group row'>
+             <div className='form-group row mt-2'>
                 <label className='col-sm-2 col-form-label'>Password</label>
-                <div className='col-sm-10'>
+                <div className='col-sm-5'>
                     <input type="password" className='form-control' name="password" placeholder="Password" />
                 </div>
              </div>
             
-           <div className='form-group row'>
+           <div className='form-group row mt-2'>
                 <label className='col-sm-2 col-form-label'>Phone Number</label>
-                <div className='col-sm-10'>
+                <div className='col-sm-5'>
                     <input type="tel" className='form-control' name="phonenumber" placeholder="Phone Number" />
                 </div>
              </div>
             
-            <div className='form-group'>
+            <div className='form-group mt-3'>
                 <input type="checkbox"  name="terms" placeholder="Terms and conditions" />
-                <label>Terms and conditions</label>
+                <label > I have agree the Terms and conditions</label>
             </div>
 
-            <button className="btn bg-primary">Submit</button>
+            <button className="btn text-light bg-danger fg-light mt-3">Submit</button>
         </form>
         </div>
     )
