08597e4ee2a580caea44b1dc2fa3ccb304df3c7e
aslin-r <aslinjoelr123@gmail.com>
2023-05-21 23:26:24 +0530

    added basic bootstrap

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,26 +1,51 @@
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 export default function Register(){
     return(
-        <div className="container">
-            <h1>Create an Account </h1>
+        <div className="container p-3 shadow  mt-5 bg-white rounded">
+            <h3>Create an Account </h3>
+
         <form>
-            <label>First name</label>
-            <input type="text" name="firstname" placeholder="Username" />
-            <br/>
-            <label>Last name</label>
-            <input type="text" name="lastname" placeholder="Last Name" />
-            <br/>
-            <label>Email</label>
-            <input type="email" name="email" placeholder="Email" />
-            <br/>
-            <label>Password</label>
-            <input type="password" name="password" placeholder="Password" />
-            <br/>
-            <label>Phone Number</label>
-            <input type="tel" name="phone" placeholder="Phone Number" max='' />
-            <br/>
-            <input type="checkbox" name="terms" placeholder="Terms and conditions" />
-            <label>Terms and conditions</label>
-            <br/>
+
+            <div className='form-group row'>
+                <label className='col-sm-2 col-form-label'>First name</label>
+                <div className='col-sm-10'>
+                    <input type="text" className='form-control' name="firstname" placeholder="First name" />
+                </div>
+             </div>
+            
+             <div className='form-group row'>
+                <label className='col-sm-2 col-form-label'>Last name</label>
+                <div className='col-sm-10'>
+                    <input type="text" className='form-control' name="lastname" placeholder="Last name" />
+                </div>
+             </div>
+            
+             <div className='form-group row'>
+                <label className='col-sm-2 col-form-label'>Email</label>
+                <div className='col-sm-10'>
+                    <input type="email" className='form-control' name="email" placeholder="Email name" />
+                </div>
+             </div>
+
+             <div className='form-group row'>
+                <label className='col-sm-2 col-form-label'>Password</label>
+                <div className='col-sm-10'>
+                    <input type="password" className='form-control' name="password" placeholder="Password" />
+                </div>
+             </div>
+            
+           <div className='form-group row'>
+                <label className='col-sm-2 col-form-label'>Phone Number</label>
+                <div className='col-sm-10'>
+                    <input type="tel" className='form-control' name="phonenumber" placeholder="Phone Number" />
+                </div>
+             </div>
+            
+            <div className='form-group'>
+                <input type="checkbox"  name="terms" placeholder="Terms and conditions" />
+                <label>Terms and conditions</label>
+            </div>
+
             <button className="btn bg-primary">Submit</button>
         </form>
         </div>
