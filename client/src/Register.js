3575ad8968723349b857d1b5ae5cca3e6dedc6c4
aslin-r <aslinjoelr123@gmail.com>
2023-05-22 21:40:58 +0530

    created a sample register page

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,54 +1,54 @@
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
-import "./App.css"
+import "./Register.css"
 export default function Register(){
     return(
         
-        <div className='container'>
+        <div class='container'>
             
 
-        <form className=" p-3 shadow bg-white rounded mt-5">
+        <form class=" p-3 shadow bg-white rounded mt-5">
             <h3><center>Create an Account</center> </h3>
-            <div className='form-group row mt-4'>
-                <label className='col-sm-2 col-form-label'>First name</label>
-                <div className='col-sm-5'>
-                    <input type="text" className='form-control' name="firstname" placeholder="First name" />
+            <div class='form-group row mt-4'>
+                <label class='col-sm-2 col-form-label'>First name</label>
+                <div class='col-sm-5'>
+                    <input type="text" class='form-control' name="firstname" placeholder="First name" />
                 </div>
              </div>
             
-             <div className='form-group row mt-2'>
-                <label className='col-sm-2 col-form-label'>Last name</label>
-                <div className='col-sm-5'>
-                    <input type="text" className='form-control' name="lastname" placeholder="Last name" />
+             <div class='form-group row mt-2'>
+                <label class='col-sm-2 col-form-label'>Last name</label>
+                <div class='col-sm-5'>
+                    <input type="text" class='form-control' name="lastname" placeholder="Last name" />
                 </div>
              </div>
             
-             <div className='form-group row mt-2'>
-                <label className='col-sm-2 col-form-label'>Email</label>
-                <div className='col-sm-5'>
-                    <input type="email" className='form-control' name="email" placeholder="Email name" />
+             <div class='form-group row mt-2'>
+                <label class='col-sm-2 col-form-label'>Email</label>
+                <div class='col-sm-5'>
+                    <input type="email" class='form-control' name="email" placeholder="Email name" />
                 </div>
              </div>
 
-             <div className='form-group row mt-2'>
-                <label className='col-sm-2 col-form-label'>Password</label>
-                <div className='col-sm-5'>
-                    <input type="password" className='form-control' name="password" placeholder="Password" />
+             <div class='form-group row mt-2'>
+                <label class='col-sm-2 col-form-label'>Password</label>
+                <div class='col-sm-5'>
+                    <input type="password" class='form-control' name="password" placeholder="Password" />
                 </div>
              </div>
             
-           <div className='form-group row mt-2'>
-                <label className='col-sm-2 col-form-label'>Phone Number</label>
-                <div className='col-sm-5'>
-                    <input type="tel" className='form-control' name="phonenumber" placeholder="Phone Number" />
+           <div class='form-group row mt-2'>
+                <label class='col-sm-2 col-form-label'>Phone Number</label>
+                <div class='col-sm-5'>
+                    <input type="tel" class='form-control' name="phonenumber" placeholder="Phone Number" />
                 </div>
              </div>
             
-            <div className='form-group mt-3'>
-                <input className='check' type="checkbox"  name="terms" placeholder="Terms and conditions" />
+            <div class='form-group mt-3'>
+                <input class='check' type="checkbox"  name="terms" placeholder="Terms and conditions" />
                 <label> I have agree the Terms and conditions</label>
             </div>
 
-            <button className="btn text-light bg-danger fg-light mt-3">Submit</button>
+            <button class="btn text-light bg-danger fg-light mt-3">Submit</button>
         </form>
         </div>
     )
