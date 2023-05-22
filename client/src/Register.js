e1f5a238c54a7bd1d43b72dfa1415c006bae9782
aslin-r <aslinjoelr123@gmail.com>
2023-05-22 20:40:11 +0530

    looks some more better

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,28 +1,31 @@
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
 export default function Register(){
     return(
-        <div className="container p-3 shadow  mt-5 bg-white rounded">
-            <h3>Create an Account </h3>
+        
+        <div className='container'>
+            
 
-        <form>
-
-            <div className='form-group row mt-2'>
-                
-                <div className='col-md-5 mb-3'>
-                <label className=' col-form-label'>First name</label>
+        <form className=" p-3 shadow bg-white rounded mt-5">
+            <h3><center>Create an Account</center> </h3>
+            <div className='form-group row mt-4'>
+                <label className='col-sm-2 col-form-label'>First name</label>
+                <div className='col-sm-5'>
                     <input type="text" className='form-control' name="firstname" placeholder="First name" />
                 </div>
-             
-             <div className='col-md-5 mb-3'>
-                <label className=' col-form-label'>Last name</label>
-                    <input type="text" className='form-control' name="lastname" placeholder="Last Name" />
+             </div>
+            
+             <div className='form-group row mt-2'>
+                <label className='col-sm-2 col-form-label'>Last name</label>
+                <div className='col-sm-5'>
+                    <input type="text" className='form-control' name="lastname" placeholder="Last name" />
                 </div>
              </div>
             
              <div className='form-group row mt-2'>
                 <label className='col-sm-2 col-form-label'>Email</label>
                 <div className='col-sm-5'>
-                    <input type="email" className='form-control' name="email" placeholder="Email" />
+                    <input type="email" className='form-control' name="email" placeholder="Email name" />
                 </div>
              </div>
 
@@ -41,8 +44,8 @@ export default function Register(){
              </div>
             
             <div className='form-group mt-3'>
-                <input type="checkbox"  name="terms" placeholder="Terms and conditions" />
-                <label > I have agree the Terms and conditions</label>
+                <input className='check' type="checkbox"  name="terms" placeholder="Terms and conditions" />
+                <label> I have agree the Terms and conditions</label>
             </div>
 
             <button className="btn text-light bg-danger fg-light mt-3">Submit</button>
