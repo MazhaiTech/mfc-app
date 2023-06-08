4d7fe7967edd3238cb107c02cb0d391700426c3c
aslin-r <aslinjoelr123@gmail.com>
2023-06-08 22:54:06 +0530

    upadated the register page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -4,34 +4,34 @@ export default function Register() {
   return (
     <center>
       <form>
-      <h2 className="acc">
-        Register your account
-      </h2>
-      <div>
-      <label className="mn">First name</label>
-      <input className="line-input1" type="text" name="firstname" placeholder="First name" />
-      <br/>
-      <label className="mn">Last name</label>
-      <input className="line-input1" type="text" name="lastname" placeholder="Last name" />
-      <br/>
-      <label className="mn">Email</label>
-      <input className="line-input1" type="email" name="email" placeholder="Email name" />
-      <br/>
-      <label className="mn">Password</label>
-      <input className="line-input1" type="password" name="password" placeholder="Password" />
-      <br/>
-      <label className="mn">Confirm Password</label>
-      <input className="line-input1" type="password" name="password2" placeholder="Confirm Password" />
-      <br/>
-      <label className="mn">Address</label>
-      <input className="line-input1" type="text" name="address" placeholder="Address" />
-      <br/>
-      <input className="" type="checkbox" row="5" cols="5" name="terms" placeholder="Terms and conditions" />
-      <label className="checkbox-label">I have agreed to the <a href="">Terms and conditions</a></label>
-      </div>
-      <br/>
-      <button>Submit</button>
-    </form>
+        <h2 className="heading">
+          Register your account
+        </h2>
+        <div>
+          <label className="labels">First name</label>
+          <input className="line-input1" type="text" name="firstname" placeholder="First name" />
+          <br/>
+          <label className="labels">Last name</label>
+          <input className="line-input1" type="text" name="lastname" placeholder="Last name" />
+          <br/>
+          <label className="labels">Email</label>
+          <input className="line-input1" type="email" name="email" placeholder="Email name" />
+          <br/>
+          <label className="labels">Password</label>
+          <input className="line-input1" type="password" name="password" placeholder="Password" />
+          <br/>
+          <label className="labels">Confirm Password</label>
+          <input className="line-input1" type="password" name="password2" placeholder="Confirm Password" />
+          <br/>
+          <label className="labels label-address">Address</label>
+          <textarea name='address' placeholder='Enter your address'  className='line-input1 area'></textarea>
+          <br/>
+          <input className="" type="checkbox" name="terms" placeholder="Terms and conditions" />
+          <label className="checkbox-label">I have agreed to the <a href="">Terms and conditions</a></label>
+        </div>
+        <br/>
+        <button>Submit</button>
+      </form>
     </center>
   );
 }
