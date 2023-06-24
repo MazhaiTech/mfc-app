9d7a1cfd847125ee1144d305f5b10df5fc5b0435
aslin-r <aslinjoelr123@gmail.com>
2023-06-24 12:26:48 +0530

    updated register page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,32 +5,33 @@ export default function Register() {
     <center>
       <form>
         <h2 className="heading">
-          Register your account
+          Sign Up
         </h2>
         <div>
-          <label className="labels">First name</label>
-          <input className="line-input1" type="text" name="firstname" placeholder="First name" />
+
+          <label className="labels">Name</label>
+          <input className="line-input1" type="text" name="firstname" placeholder="Name" />
           <br/>
-          <label className="labels">Last name</label>
-          <input className="line-input1" type="text" name="lastname" placeholder="Last name" />
-          <br/>
+         
           <label className="labels">Email</label>
           <input className="line-input1" type="email" name="email" placeholder="Email" />
           <br/>
+
           <label className="labels">Password</label>
           <input className="line-input1" type="password" name="password" placeholder="Password" />
           <br/>
-          <label className="labels">Confirm Password</label>
-          <input className="line-input1" type="password" name="password2" placeholder="Confirm Password" />
-          <br/>
+
           <label className="labels label-address">Address</label>
+
           <textarea name='address' placeholder='Enter your address'  className='line-input1 area'></textarea>
           <br/>
+
           <input className="" type="checkbox" name="terms" placeholder="Terms and conditions" />
           <label className="checkbox-label">I have agreed to the <a href="">Terms and conditions</a></label>
+        
         </div>
         <br/>
-        <button>Submit</button>
+        <button>Continue</button>
       </form>
     </center>
   );
